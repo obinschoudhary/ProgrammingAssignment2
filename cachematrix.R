f75ccd2405a034d6480ed58dce4cc97bd3f4ada3
Obs <obinsanni@gmail.com>
2014-09-21 23:25:04 +0200

    Coursera - R programming; assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Creates a special matrix object that can cache its inverse
+makeCacheMatrix <- function (m = matrix()) {
+    
+    ## Initialize the inverse property
+    i <- NULL
+    
+    ## Method to set the matrix
+    set <- function(matrix){
+        m <<- matrix
+        i <<- NULL
+    }
+    
+    ## Method the get the matrix
+    get <- function() m
+    
+    ## Method to set the inverse of the matrix
+    setMatInv <- function(MatInverse) i <<- MatInverse
+    
+    ## Method to get the inverse of the matrix
+    getMatInv <- function() i
+    
+    ## Return a list of the methods
+    mypOptMatrix <<- list(get=get,setMatInv=setMatInv,getMatInv=getMatInv)
+    mypOptMatrix
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## If the inverse has already been calculated (and the matrix has not
+## changed), then the "cachesolve" should retrieve the inverse from the cache.
+cacheSolve <- function(x = myOptMatrix, ...) {
+    
+    ## Return a matrix that is the inverse of 'x'
+    MatInverse<- x$getMatInv()
+    
+    ## Return the inverse if its already set    
+    if(!is.null(MatInverse)){
+        message("getting cached matrix inverse")
+        return(MatInverse)    
+    }
+    
+    ## Get the matrix from our object
+    DataMatrix<-x$get()
+    
+    ## Calculate the inverse using matrix multiplication
+    MatInverse<-solve(DataMatrix,...)
+    
+    ## Set the inverse to the object
+    x$setMatInv(MatInverse)
+    MatInverse}
